5174096be7137ba9378d98c90fdd0def6a400275
Tomasz Grabarczyk <32244889+tomasz-grabarczyk@users.noreply.github.com>
2018-08-13 18:42:34 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package simpleCalculatorGUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleCalculatorGUI simpleCalculatorGUI = new SimpleCalculatorGUI();
+        simpleCalculatorGUI.buildCalculatorGUI();
+    }
+}
